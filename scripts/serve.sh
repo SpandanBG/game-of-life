1181f8e66db3400c7da276c24a81fccab1479c02
Spandan Buragohain <spandan.buragohain@thoughtworks.com>
2019-05-05 15:21:40 +0530

    [Spandan] Emptied project files

--- scripts/serve.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# ---------------------- DEV SERVER ----------------------
-function run_dev_server() {
-    webpack-dev-server --progress --color --inline --hot
-}
-
-# ----- main -----
-run_dev_server
